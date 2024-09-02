a1cc0761400df492f6cb18eb847b1e6ca86b4dd9
Rananjay Singh <rananjaysingh20@gmail.com>
2024-09-03 01:56:49 +0530

    added styles to expand the card when clicked

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,6 +3,8 @@ import "../styles/card.css"; // Import the CSS file
 
 const Card = ({ item, addToCart }) => {
   const [quantity, setQuantity] = useState(1);
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const increaseQuantity = () => {
     setQuantity(quantity + 1);
   };
@@ -12,30 +14,46 @@ const Card = ({ item, addToCart }) => {
       setQuantity(quantity - 1);
     }
   };
+
+  const expandCard = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className="product-card-container">
-      <div className="card">
-        <div className="image-container">
-          {item.img.map((image, key) => {
-            return (
-              <div key={key}>
-                <img src={image} alt={image} className="card-image" />
-              </div>
-            );
-          })}
+    <div
+      className={`product-card-container ${isExpanded ? "removeHover" : ""}`}
+    >
+      <div className={`${isExpanded ? "overlay" : ""}`}>
+        <div
+          className={`expanded-card-container ${
+            isExpanded ? "expandedCard" : ""
+          }`}
+        >
+          <div className="card">
+            <div className="image-container">
+              {item.img.map((image, key) => {
+                return (
+                  <div key={key}>
+                    <img src={image} alt={image} className="card-image" />
+                  </div>
+                );
+              })}
+            </div>
+            <div className="card-title">{item.title}</div>
+            <div className="card-description">{item.description}</div>
+          </div>
+          <button onClick={expandCard}>epxand</button>
+          <div className="controls-container">
+            <span>Rs. {item.price}</span>
+            <div className="quantity-controls">
+              <button onClick={decreaseQuantity}>-</button>
+              <span>{quantity}</span>
+              <button onClick={increaseQuantity}>+</button>
+            </div>
+          </div>
+          <button onClick={() => addToCart(item, quantity)}>Add to Cart</button>
         </div>
-        <div className="card-title">{item.title}</div>
-        <div className="card-description">{item.description}</div>
       </div>
-      <div className="controls-container">
-        <span>Rs. {item.price}</span>
-        <div className="quantity-controls">
-          <button onClick={decreaseQuantity}>-</button>
-          <span>{quantity}</span>
-          <button onClick={increaseQuantity}>+</button>
-        </div>
-      </div>
-      <button onClick={() => addToCart(item, quantity)}>Add to Cart</button>
     </div>
   );
 };
